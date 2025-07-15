1caaba3f89c2f2dab3b6683a2c14c907100383f9
leonardobasso <parliamodiarte1@gmail.com>
2025-07-15 15:13:19 +0200

    compiled for RPM based distros

---
+++ bin/rpm/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Downloading public key from Codeberg..."
+curl -s -o key.pub https://codeberg.org/leonardobasso/pubkeys/raw/branch/main/rpm/key.pub
+
+echo "Importing public key..."
+sudo rpm --import key.pub
+
+echo "Installing the rpm..."
+sudo rpm -i ./book-recommender-0.1.0-1.x86_64.rpm
+
